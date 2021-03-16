4766f59f2d11cebc2899616e08f5d837fdafdff5
xydaozhu <55057386+xydaozhu@users.noreply.github.com>
2021-03-16 15:05:48 +0800

    Update RM2100.openwrtS.leanP.sh

--- RM2100.openwrtS.leanP.sh
+++ RM2100.openwrtS.leanP.sh
@@ -19,18 +19,19 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 git clone https://github.com/coolsnowwolf/lede.git
 mkdir /workdir/openwrt/package/lean
-cp -r lede/package/lean/adbyby  /workdir/openwrt/package/lean/adbyby
-cp -r lede/package/lean/vlmcsd  /workdir/openwrt/package/lean/vlmcsd
-cp -r lede/package/lean/luci-app-vlmcsd  /workdir/openwrt/package/lean/luci-app-vlmcsd
-cp -r lede/package/lean/luci-app-adbyby-plus /workdir/openwrt/package/lean/luci-app-adbyby-plus
-cp -r lede/package/lean/luci-app-sfe  /workdir/openwrt/package/lean/luci-app-sfe
-cp -r lede/package/lean/luci-app-ipsec-vpnd /workdir/openwrt/package/lean/luci-app-ipsec-vpnd
-cp -r lede/package/lean/UnblockNeteaseMusic /workdir/openwrt/package/lean/UnblockNeteaseMusic
-cp -r lede/package/lean/UnblockNeteaseMusicGo /workdir/openwrt/package/lean/UnblockNeteaseMusicGo
-cp -r lede/package/lean/uugamebooster /workdir/openwrt/package/lean/uugamebooster
+cp -r lede/package/lean  /workdir/openwrt/package/lean
+# cp -r lede/package/lean/adbyby  /workdir/openwrt/package/lean/adbyby
+# cp -r lede/package/lean/vlmcsd  /workdir/openwrt/package/lean/vlmcsd
+# cp -r lede/package/lean/luci-app-vlmcsd  /workdir/openwrt/package/lean/luci-app-vlmcsd
+# cp -r lede/package/lean/luci-app-adbyby-plus /workdir/openwrt/package/lean/luci-app-adbyby-plus
+# cp -r lede/package/lean/luci-app-sfe  /workdir/openwrt/package/lean/luci-app-sfe
+# cp -r lede/package/lean/luci-app-ipsec-vpnd /workdir/openwrt/package/lean/luci-app-ipsec-vpnd
+# cp -r lede/package/lean/UnblockNeteaseMusic /workdir/openwrt/package/lean/UnblockNeteaseMusic
+# cp -r lede/package/lean/UnblockNeteaseMusicGo /workdir/openwrt/package/lean/UnblockNeteaseMusicGo
+# cp -r lede/package/lean/uugamebooster /workdir/openwrt/package/lean/uugamebooster
 # cp -r lede/package/lean/luci-app-zerotier /workdir/openwrt/package/lean/luci-app-zerotier
-cp -r lede/package/lean/luci-theme-argon /workdir/openwrt/package/lean/luci-theme-argon
-cp -r lede/package/lean/luci-theme-netgear /workdir/openwrt/package/lean/luci-theme-netgear
+# cp -r lede/package/lean/luci-theme-argon /workdir/openwrt/package/lean/luci-theme-argon
+# cp -r lede/package/lean/luci-theme-netgear /workdir/openwrt/package/lean/luci-theme-netgear
 # git clone https://github.com/kenzok8/openwrt-packages
 # cp -r openwrt-packages/luci-app-ssr-plus /workdir/openwrt/package/lean/luci-app-ssr-plus
 # cp -r openwrt-packages/luci-app-passwall /workdir/openwrt/package/lean/luci-app-passwall
