f91b4038f80b08bf49bb6785a122623c40c6e622
xydaozhu <55057386+xydaozhu@users.noreply.github.com>
2021-03-17 23:28:13 +0800

    Update RM2100.openwrtS.leanP.sh

--- RM2100.openwrtS.leanP.sh
+++ RM2100.openwrtS.leanP.sh
@@ -55,14 +55,14 @@ find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -
 #svn co https://github.com/immortalwrt/immortalwrt/trunk/package/lean/v2ray package/lean/v2ray
 # git clone https://github.com/xiaorouji/openwrt-passwall package/lean/openwrt-passwall
 # Add Jerrykuku's packages(vssr/jd-daily/argon theme)
-rm -rf package/lean/luci-theme-argon
-rm -rf package/lean/luci-app-jd-dailybonus
-git clone https://github.com/jerrykuku/lua-maxminddb package/jerrykuku/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-argon-config package/jerrykuku/luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/lean/luci-app-jd-dailybonus
+# rm -rf package/lean/luci-theme-argon
+# rm -rf package/lean/luci-app-jd-dailybonus
+# git clone https://github.com/jerrykuku/lua-maxminddb package/jerrykuku/lua-maxminddb
+# git clone https://github.com/jerrykuku/luci-app-argon-config package/jerrykuku/luci-app-argon-config
+# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/lean/luci-app-jd-dailybonus
 # git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
-git clone https://github.com/jerrykuku/luci-app-vssr package/jerrykuku/luci-app-vssr
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/jerrykuku/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-app-vssr package/jerrykuku/luci-app-vssr
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/jerrykuku/luci-theme-argon
 ls /workdir/openwrt/package/lean/ -l
 
 # Add Lienol's Packages
