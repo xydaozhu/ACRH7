2d26c5cc98f75cfc99c2a84c5f732fd04878c0ba
xydaozhu <55057386+xydaozhu@users.noreply.github.com>
2023-04-02 22:33:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,8 +11,8 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # 
 # Add ServerChan
-git clone --depth=1 https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
-git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
+# git clone --depth=1 https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+# git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
